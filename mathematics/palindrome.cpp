02cd187d139e73edd93dec26e3fd0d030c22c10d
Aditi Sharma <aditisharmamay22@gmail.com>
2026-01-27 18:38:25 +0530

    if a number is plaindrome or not

---
+++ mathematics/palindrome.cpp
@@ -0,0 +1,20 @@
+// whether a number is palindrome or not
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, reversedNumber = 0, remainder, originalNumber;
+    cout << "Enter an integer: ";
+    cin >> n;
+    originalNumber = n;
+    while (n != 0) {
+        remainder = n % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        n /= 10;
+    }
+    if (originalNumber == reversedNumber)
+        cout << originalNumber << " is a palindrome." << endl;
+    else
+        cout << originalNumber << " is not a palindrome." << endl;
+    return 0;
+}
